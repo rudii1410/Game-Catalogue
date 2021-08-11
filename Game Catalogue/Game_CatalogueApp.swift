d2e3c2208ee980c31f5e8f210a04e50a5925aa09
Rudiyanto <rudiyanto@bukalapak.com>
2021-08-11 12:10:27 +0700

    Initial Commit

---
+++ Game Catalogue/Game_CatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CatalogueApp.swift
+//  Game Catalogue
+//
+//  Created by Rudiyanto on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_CatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
